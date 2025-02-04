0aa02b607b0f96e765ee8ef93bb572c13a217327
Carlos Barrios <juanbarrios045@gmail.com>
2025-02-04 02:48:17 -0400

    main fix cart translate

--- src/app/my-cart/page.js
+++ src/app/my-cart/page.js
@@ -1,13 +1,28 @@
+"use client";
+import { useEffect } from "react";
 import CartSectionComponent from "@/components/organisms/CartSection";
 import Footer from "@/components/organisms/Footer";
 import Navbar from "@/components/organisms/Navbar";
 
 export default function MyCart() {
+  useEffect(() => {
+    document.querySelectorAll("*").forEach((element) => {
+      element.setAttribute("translate", "no");
+    });
+  }, []);
+
   return (
-    <main>
-      <Navbar />
-      <CartSectionComponent />
-      <Footer />
-    </main>
+    <>
+      <head>
+        <meta http-equiv="Content-Language" content="es" />
+        <meta name="google" content="notranslate" />
+      </head>
+
+      <main>
+        <Navbar />
+        <CartSectionComponent />
+        <Footer />
+      </main>
+    </>
   );
 }
